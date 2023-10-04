ed600e1c0e94486769139156578748c0ef42cb29
VasilyUA <mironenkovasila@gmail.com>
2023-10-04 15:59:34 +0300

    Change project

--- js/results.js
+++ js/results.js
@@ -1,12 +1,15 @@
 const urlParams = new URLSearchParams(window.location.search);
 const resultParam = urlParams.get("result");
+const cardsResult = document.getElementById("cards-result");
 const list = document.getElementById("list");
 const logo = document.getElementById("logo");
 
 if (resultParam === "true") {
+  cardsResult.style.display = "flex";
   list.style.display = "none";
   logo.style.display = "none";
 } else {
+  cardsResult.style.display = "none";
   list.style.display = "block";
   logo.style.display = "block";
 }
